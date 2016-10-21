4f70639ce2128e2694848df566a4f93d38e20ece
jcguardino <josh.guardino@libertymutual.com>
2016-10-21 12:03:42 -0400

    Updated test script

--- test/run.js
+++ test/run.js
@@ -12,7 +12,10 @@ assert.equal(core.config.get('myConfig-overwrite'), 'overwrite');
 assert.equal(core.config.get('myConfig-env'), 'env');
 
 // string
+assert.equal(core.string.format('the value is {0}.', 'value'), 'the value is value.');
 assert.equal(core.string.format('{0}:{1}', 'key', 'value'), 'key:value');
+assert.equal(core.string.camelCase('abc-def'), 'abcDef');
+assert.equal(core.string.pascalCase('abc-def'), 'AbcDef');
 
 // security
 var encrypted = core.security.encryptString('test', 'key');
