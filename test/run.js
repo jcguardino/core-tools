2339c85320fa4f5a910d3419e4497547e1f7d6f0
jcguardino <josh.guardino@libertymutual.com>
2016-11-24 21:30:31 -0500

    1.0.6

--- test/run.js
+++ test/run.js
@@ -13,12 +13,17 @@ assert.equal(core.config.get('myConfig-env'), 'env');
 
 // string
 assert.equal(core.string.format('the value is {0}.', 'value'), 'the value is value.');
-assert.equal(core.string.format('{0}:{1}', 'key', 'value'), 'key:value');
+assert.equal(core.string.format('{0}:{1}', ['key', 'value']), 'key:value');
+assert.equal(core.string.format('{0}:{1}:{2}', ['key', 'value1', 'value2']), 'key:value1:value2');
 assert.equal(core.string.camelCase('abc-def'), 'abcDef');
 assert.equal(core.string.pascalCase('abc-def'), 'AbcDef');
 
 // security
 var encrypted = core.security.encryptString('test', 'key');
 assert.equal(core.security.decryptString(encrypted, 'key'), 'test');
+
+// logger
+var logText = core.logger.log('{0}', 'ok', {returnValue: true});
+assert.ok(/\d+\/\d+ @ \d+:\d+:\d+ - ok/.exec(logText));
 
 console.log('All tests passed');
